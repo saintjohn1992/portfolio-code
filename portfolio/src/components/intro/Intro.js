b8f76d530a719c5a2bf581ad136769c28e2676b5
saintjohn1992 <saintjohnshane@outlook.com>
2022-05-25 20:22:06 +0200

    finished with left info div for the most part and working on right info div

--- portfolio/src/components/intro/Intro.js
+++ portfolio/src/components/intro/Intro.js
@@ -1,4 +1,5 @@
 import "./intro.css"
+import Me from "../../img/me.png"
 
 const Intro = () => {
 return (
@@ -8,19 +9,22 @@ return (
               <h2 className="i-intro">Hello, My name is</h2>
               <h1 className="i-name">Shane St. John</h1>
               <div className="intro-title">
-                  <div className="intro-wrapper-title">
+                  <div className="intro-title-wrapper">
                       <div className="intro-title-item">Software Developer</div>
                       <div className="intro-title-item">UI/UX Designer</div>
                       <div className="intro-title-item">Web Developer</div>
                       <div className="intro-title-item">Fitness Fanatic</div>
                   </div>
               </div>
+              <div className="intro-description">
+                  I use my skills to design, develop, stylish modern websites, web services, and online stores
+                  for customers. 
+              </div>
           </div>
       </div>
       <div className="intro-right">
-          <div className="intro-right-wrapper">
-
-          </div>
+          <div className="i-bg"></div>
+          <img src={Me} alt="" className="i-img" />
       </div>
   </div>
   )
