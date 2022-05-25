fafda106db61511e9b9ee4381eb24c778483ec68
saintjohn1992 <saintjohnshane@outlook.com>
2022-05-25 17:43:51 +0200

    working on intro component along with its corresponding css file

--- portfolio/src/components/intro/Intro.js
+++ portfolio/src/components/intro/Intro.js
@@ -1,9 +1,27 @@
+import "./intro.css"
 
 const Intro = () => {
 return (
   <div className="intro">
-      <div className="intro-left">left</div>
-      <div className="intro-right">right</div>
+      <div className="intro-left">
+          <div className="intro-left-wrapper">
+              <h2 className="i-intro">Hello, My name is</h2>
+              <h1 className="i-name">Shane St. John</h1>
+              <div className="intro-title">
+                  <div className="intro-wrapper-title">
+                      <div className="intro-title-item">Software Developer</div>
+                      <div className="intro-title-item">UI/UX Designer</div>
+                      <div className="intro-title-item">Web Developer</div>
+                      <div className="intro-title-item">Fitness Fanatic</div>
+                  </div>
+              </div>
+          </div>
+      </div>
+      <div className="intro-right">
+          <div className="intro-right-wrapper">
+
+          </div>
+      </div>
   </div>
   )
 }
