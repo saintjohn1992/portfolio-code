bbbc24c3f22888f1c335cb19acc1eb7b71497bde
saintjohn1992 <saintjohnshane@outlook.com>
2022-05-26 19:10:49 +0200

    added img, two cards, and still working on the css and jsx components.

--- portfolio/src/components/about/About.js
+++ portfolio/src/components/about/About.js
@@ -1,9 +1,15 @@
 import "./about.css";
+import Dev from "../../img/dev.jpg";
 
 const About = () =>  {
    return(
     <div className="a">
-    <div className="a-left"></div>
+    <div className="a-left">
+        <div className="a-card bg"></div>
+        <div className="a-card">
+            <img src={Dev} alt="" className="a-img" />
+        </div>
+    </div>
     <div className="a-right"></div>
 </div>
    )
