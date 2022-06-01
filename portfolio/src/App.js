0eb127faeac0726a0b265c5e2620a0196b7c9bb9
saintjohn1992 <saintjohnshane@outlook.com>
2022-06-01 15:14:14 +0200

    added darkmode using context api along with a toggle button

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -3,10 +3,18 @@ import Intro from './components/intro/Intro';
 import About from "./components/about/About";
 import ProductList from './components/productList/ProductList';
 import Contact from './components/contact/Contact';
+import Toggle from './components/toggle/toggle';
+import { useContext } from 'react';
+import { ThemeContext } from './context';
 
 function App() {
+
+  const theme = useContext(ThemeContext)
+  const darkMode = theme.state.darkMode;
+
   return (
-    <div className="App">
+    <div style={{ backgroundColor: darkMode ? "#222" : "white", color: darkMode && "white" }} >
+    <Toggle />  
     <Intro />
     <About />
     <ProductList />
--- portfolio/src/components/contact/Contact.js
+++ portfolio/src/components/contact/Contact.js
@@ -2,12 +2,15 @@ import "./contact.css";
 import Phone from "../../img/phone.png";
 import Email from "../../img/email.png";
 import Address from "../../img/address.png";
-import { useRef, useState } from "react";
+import { useContext, useRef, useState } from "react";
 import emailjs from '@emailjs/browser';
+import { ThemeContext } from "../../context";
 
 const Contact = () => {
     const formRef = useRef();
     const [done, setDone] = useState(false)
+    const theme = useContext(ThemeContext)
+    const darkMode = theme.state.darkMode;
 
     const handleSubmit = (e) => {
         e.preventDefault();
@@ -46,12 +49,12 @@ const Contact = () => {
                         <b>What Project do you have in mind?</b> Let's get in touch.
                     </p>
                     <form ref={formRef} onSubmit={handleSubmit}>
-                        <input type="text" placeholder="Name" name="user_name" />
+                        <input style={{backgroundColor: darkMode && "#333"}} type="text" placeholder="Name" name="user_name" />
                         <br />
-                        <input type="text" placeholder="Subject" name="user_subect" />
+                        <input style={{backgroundColor: darkMode && "#333"}}type="text" placeholder="Subject" name="user_subect" />
                         <br />
-                        <input type="email" placeholder="Email" name="user_email" />
-                        <textarea rows="5" placeholder="Message" name="message" />
+                        <input style={{backgroundColor: darkMode && "#333"}} type="email" placeholder="Email" name="user_email" />
+                        <textarea style={{backgroundColor: darkMode && "#333"}} rows="5" placeholder="Message" name="message" />
                         <button>Submit</button>
                         
                         {done && "Thank you..."}
