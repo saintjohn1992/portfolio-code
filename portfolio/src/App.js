5e441562acbb9e41b207ffaae7093fd6b7c9d7bb
saintjohn1992 <saintjohnshane@outlook.com>
2022-06-01 11:05:17 +0200

    started creating contact component along with css file

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import Intro from './components/intro/Intro';
 import About from "./components/about/About";
 import ProductList from './components/productList/ProductList';
+import Contact from './components/contact/Contact';
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
     <Intro />
     <About />
     <ProductList />
+    <Contact />
     </div>
   );
 }
---
+++ portfolio/src/components/contact/Contact.js
@@ -0,0 +1,15 @@
+import "./contact.css";
+
+const Contact = () => {
+    return (
+        <div className="c">
+            <div className="c-bg"></div>
+            <div className="c-wrapper">
+                <div className="c-left"></div>
+                <div className="c-right"></div>
+            </div>
+        </div>
+    )
+    }
+
+    export default Contact; 
